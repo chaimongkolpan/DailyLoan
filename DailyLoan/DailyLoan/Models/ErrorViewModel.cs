3f7676482a22d5a69e1dbd554099c329cfb26e1d
chaimongkolpan <cpanawasri@gmail.com>
2021-11-03 10:25:13 +0700

    init project

---
+++ DailyLoan/DailyLoan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DailyLoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
