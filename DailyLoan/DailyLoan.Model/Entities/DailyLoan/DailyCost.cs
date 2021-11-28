28798e8c493b52d9d9810c992ed9b7002c6ba7bd
chaimongkolpan <cpanawasri@gmail.com>
2021-11-28 19:52:07 +0700

    pretest update

--- DailyLoan/DailyLoan.Model/Entities/DailyLoan/DailyCost.cs
+++ DailyLoan/DailyLoan.Model/Entities/DailyLoan/DailyCost.cs
@@ -44,5 +44,7 @@ namespace DailyLoan.Model.Entities.DailyLoan
         public DateTime CreateDate { get; set; }
         public int? UpdateBy { get; set; }
         public DateTime? UpdateDate { get; set; }
+        public double? OtherIncome { get; set; }
+        public string OtherIncomeRemark { get; set; }
     }
 }
